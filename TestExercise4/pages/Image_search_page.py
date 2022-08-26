12748c8e5c196450063603fb5414687abaa8c2c1
SirVsevolod <19vsevolod97@gmail.com>
2022-08-26 15:46:24 +0300

    without error

--- TestExercise4/pages/Image_search_page.py
+++
@@ -1,8 +0,0 @@
-from .base_page import BasePage
-from .locators import ImageSearchLocators
-
-
-class ImageSearchPage(BasePage):
-    def get_value_from_search_panel(self):
-        element = self.wait_visible(ImageSearchLocators.SEARCH_PANEL)
-        return element.get_attribute("value")
